17559641f24b0ab1468498d2a595eb903c1127b8
Tiago Matana <tiago.matana@gmail.com>
2017-10-04 08:02:27 -0300

    email test

---
+++ email.php
@@ -0,0 +1,23 @@
+<?php
+ 
+ini_set ('display_errors', 1);
+ 
+error_reporting (E_ALL);
+ 
+$from = "acaorua@acompar.org";
+ 
+$to = "tiago.matana@gmail.com";
+ 
+$subject = "Verificando o correio do PHP";
+ 
+$message = "O correio do PHP funciona bem";
+ 
+$headers = 'From: administrativo@acompar.org' . "\r\n" .
+'Reply-To: contato@tiagomatana.com' . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+ 
+mail($to, $subject, $message, $headers);
+ 
+echo "A mensagem de e-mail foi enviada.";
+ 
+?>
